a3b7d0345aa178104bf349e170ae0350f74c9c56
Minadarabi <minaadabirii@gmail.com>
2024-02-29 09:50:16 -0800

    last commit

--- src/Components/Color.jsx
+++ src/Components/Color.jsx
@@ -3,8 +3,8 @@ import "./color.css"
 
 
 export const Color = () => {
-    const [colorHex , setColorHex] = useState("#000000");
-    const [colorRgb , setColorRgb] = useState("255");
+    const [color , setColor] = useState("#000000");
+    const [colorType , setColorType] = useState("Hex");
     
     function createHex(){
         const letterHex = "0123456789ABCDEF";
@@ -12,26 +12,45 @@ export const Color = () => {
 
         for (let index = 0; index < 6; index++) {
            newHex += letterHex[Math.floor(Math.random()* 16)]
-           console.log(newHex)
+           
             
         }
-        setColorHex(newHex)
+        setColor(newHex)
+        setColorType("Hex")
     };
+    function createRgb(){
+        const numberRgbRed = (Math.floor(Math.random() * 256));
+        const numberRgbGreen = (Math.floor(Math.random() * 256));
+        const numberRgbBlue = (Math.floor(Math.random() * 256));
+        const rgb = `rgb(${numberRgbRed} , ${numberRgbGreen} , ${numberRgbBlue})`;
+
+        setColor(rgb)
+        setColorType("Rgb")
+        
+    }
+    function randomColor(){
+        const randomNum = (Math.floor(Math.random() * 2));
+        if(randomNum === 0){
+            createHex()
+        }else{
+            createRgb()
+        }
+    }
 
     return (
     <>
     
-    <div className="btn-container" style={{background: colorHex}}>
+    <div className="btn-container" style={{background: color}}  >
         <div className="btns">
           <button onClick={createHex}>Create HEX color</button>
-          <button>Create RGB color</button>
-         <button>Generate random color</button>
+          <button onClick={createRgb}>Create RGB color</button>
+         <button onClick={randomColor}>Generate random color</button>
 
         </div>
       
         <div className="text">
-            <h2>Hex Color</h2>
-            <h1>{colorHex}</h1>
+            <h2>{`${colorType} Color`}</h2>
+            <h1>{color}</h1>
 
         </div>
      
